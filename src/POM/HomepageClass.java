f9d0fcb1c95f88d46199ade3d3f3447bde1fdac0
Ashishbahurupi <103589647+Ashishbahurupi@users.noreply.github.com>
2022-04-15 08:59:20 +0530

    Update HomepageClass.java

--- src/POM/HomepageClass.java
+++ src/POM/HomepageClass.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//@ change
 public class HomepageClass {
 
     @FindBy(id="//*[@id='welcome']") private WebElement link;
